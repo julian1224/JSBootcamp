6a012f39d3fc695c62f2d38945211ce2d9ee3cd8
Dio Brando <7iscool77@gmail.com>
2017-09-19 12:30:33 +0200

    Revert "up to 14."
    This reverts commit 14163d97dc60436f9b862094df0e4afe39d84e79.

--- noEight.js
+++ noEight.js
@@ -3,9 +3,7 @@
     function multiply(a, b) {
   return a * b;
 }
-    function concatName(a, b) {
-      return a + " " + b;
-    }
+
 
       function noEight() {
         console.log("Yo. It is I.");
@@ -13,9 +11,4 @@
             var x = Number(prompt("Enter first number:"));
           var y =  Number(prompt("Enter the second number:"));
         document.getElementById("testText").innerHTML = multiply(y, x);
-      console.log("Very good! The answer is embedded in the page.")
-    console.log("Let's do that again. Give me a first and a last name.")
-  var firstN = String(prompt("First name?"));
-var lastN = String(prompt("Last name?"));
-  document.getElementById("testText").innerHTML = concatName(firstN, lastN);
       }
--- noTwelve.js
+++
@@ -1,4 +0,0 @@
-function noTwelve() {
-  var msg = "I am a message. I am not in a global scope.";
-  document.getElementById("testText").innerHTML = msg;
-}
